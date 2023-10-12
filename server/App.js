798253d6be080321af8a5a15a72c131d8128a1d6
Riad zakaria <124533233+riadZk@users.noreply.github.com>
2023-10-12 17:11:12 +0100

    Update App.js

--- server/App.js
+++ server/App.js
@@ -5,7 +5,13 @@ const  cors= require('cors');
 dbConnect();
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: [""],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
 
 // Test
