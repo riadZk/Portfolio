3e091b497f16ad37ff0723629829f227698df8bd
Riad zakaria <124533233+riadZk@users.noreply.github.com>
2023-10-12 01:52:47 +0100

    Update file

--- server/App.js
+++ server/App.js
@@ -4,15 +4,15 @@ require('dotenv').config();
 const  cors= require('cors');
 dbConnect();
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-
 // Skills
 app.use('/api/skills',require('./routes/skills'));
+
 // Projects
 app.use('/api/projects',require('./routes/projects'));
-
 
 const PORT = process.env.PORT;
 app.listen(PORT, (req, res)=>{
