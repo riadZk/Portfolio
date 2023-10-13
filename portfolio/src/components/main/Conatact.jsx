344ad693e0bb1ce4244fc75e006aa6dcf9974821
Riad zakaria <124533233+riadZk@users.noreply.github.com>
2023-10-13 23:54:25 +0100

    Update Conatact.jsx

--- portfolio/src/components/main/Conatact.jsx
+++ portfolio/src/components/main/Conatact.jsx
@@ -46,7 +46,6 @@ const Conatact = () => {
           <h2 className='font-[400] text-center dark:text-smallTextColor text-white text-[1.5rem] '>How can I help you ?</h2>
           <div className='md:flex justify-center items-center'>
             <div className='w-full rounded-lg  md:w-1/2 sm:h-[450px] lg:flex items-center px-4 lg:px-8 '>
-
               <form ref={form} onSubmit={sendEmail} className='w-full' >
                 <div className='mb-5'>
                   <input  type='text' 
@@ -69,20 +68,20 @@ const Conatact = () => {
                           onChange={(e)=>setSubject(e.target.value)}
                           placeholder='Subject'
                           className='w-full p-3 rounded-lg text-black border-[.2px] border-black' required/>
-                </div>
-                <div className='mb-5'>
-                  <textarea
-                    type='text'
-                    name="message"
-                    rows={3}
-                    placeholder='Your Message'
-                    value={message}
-                    onChange={(e)=>{setMessage(e.target.value)}}
-                    className='w-full p-3 border-[.2px] text-black border-black rounded-lg' required/>
-                </div>
-                <button type='submit' className='w-full p-3 rounded-lg bg-color2 text-white ease-in duration-700 hover:bg-hovercolor text-center'>
-                  Send Message <i class="ri-send-plane-2-line"></i>
-                </button>
+                        </div>
+                        <div className='mb-5'>
+                          <textarea
+                            type='text'
+                            name="message"
+                            rows={3}
+                            placeholder='Your Message'
+                            value={message}
+                            onChange={(e)=>{setMessage(e.target.value)}}
+                            className='w-full p-3 border-[.2px] text-black border-black rounded-lg' required/>
+                        </div>
+                    <button type='submit' className='w-full p-3 rounded-lg bg-color2 text-white ease-in duration-700 hover:bg-hovercolor text-center'>
+                      Send Message <i class="ri-send-plane-2-line"></i>
+                    </button>
               </form>
             </div>
           </div>
