3f7a1bc4f7cf4092d124b1f3e916b78cfa4cc039
Riad zakaria <124533233+riadZk@users.noreply.github.com>
2023-10-13 22:48:46 +0100

    Update Conatact.jsx

--- portfolio/src/components/main/Conatact.jsx
+++ portfolio/src/components/main/Conatact.jsx
@@ -1,8 +1,43 @@
-import React, { useContext } from 'react'
+import React, {useRef, useContext, useState } from 'react'
 import { ModeContext } from '../../context/Context'
-
+import emailjs from '@emailjs/browser';
+import Swal from "sweetalert2";
+import { MdOutlineSubject } from 'react-icons/md';
 const Conatact = () => {
+  const form = useRef();
+  const [user,setUser]=useState('');
+  const [email,setEmail]=useState('');
+  const [subject ,setSubject]=useState('')
+  const [message,setMessage]=useState('');
   const { darkMode } = useContext(ModeContext)
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(
+          'service_a16uxsp', 
+          'template_g4g5dth',
+           form.current, 
+           'KVk7xI5u8oiavq90K'
+          )
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+            Swal.fire({
+              position: 'top-end',
+              background: "white",
+              color: "green",
+              width: "40vh",
+              text: 'Message send',
+              showConfirmButton: false,
+              timer: 1000
+          });
+          setUser('')
+          setEmail('')
+          setSubject('')
+          setMessage('')
+        }, function(err) {
+            console.log('FAILED...', err);
+        });
+  };
+
   return (
     <div className={darkMode && 'dark'}>
       <section id='conatact' className='pb-16 dark:bg-white bg-smallTextColor text-white dark:text-smallTextColor'>
@@ -11,27 +46,41 @@ const Conatact = () => {
           <h2 className='font-[400] text-center dark:text-smallTextColor text-white text-[1.5rem] '>How can I help you ?</h2>
           <div className='md:flex justify-center items-center'>
             <div className='w-full rounded-lg  md:w-1/2 sm:h-[450px] lg:flex items-center px-4 lg:px-8 '>
-              <form className='w-full'>
+
+              <form ref={form} onSubmit={sendEmail} className='w-full' >
                 <div className='mb-5'>
-                  <input type='text' placeholder='Enter Your Name'
-                    className='w-full p-3 focus:outline-none rounded-lg text-black  border-[.2px] border-black' />
+                  <input  type='text' 
+                          value={user}
+                          onChange={(e)=>{setUser(e.target.value)}}
+                          placeholder='Your Name'
+                          className='w-full p-3 rounded-lg text-black  border-[.2px] border-black' name="user_name" required/>
                 </div>
                 <div className='mb-5'>
-                  <input type='email' placeholder='Enter Your email'
-                    className='w-full p-3 focus:outline-none rounded-lg text-black border-[.2px] border-black' />
+                  <input  type='email' 
+                          placeholder='Your email'
+                          value={email}
+                          onChange={(e)=>{setEmail(e.target.value)}}
+                          className='w-full p-3 rounded-lg text-black border-[.2px] border-black' 
+                          name="user_email" required/>
                 </div>
                 <div className='mb-5 text-black'>
-                  <input type='text' placeholder='Enter Your Subjet'
-                    className='w-full p-3 focus:outline-none rounded-lg text-black border-[.2px] border-black' />
+                  <input  type='text' 
+                          value={subject}
+                          onChange={(e)=>setSubject(e.target.value)}
+                          placeholder='Subject'
+                          className='w-full p-3 rounded-lg text-black border-[.2px] border-black' required/>
                 </div>
                 <div className='mb-5'>
                   <textarea
                     type='text'
+                    name="message"
                     rows={3}
-                    placeholder='Enter Your Message'
-                    className='w-full p-3 focus:outline-none border-[.2px] text-black border-black rounded-lg' />
+                    placeholder='Your Message'
+                    value={message}
+                    onChange={(e)=>{setMessage(e.target.value)}}
+                    className='w-full p-3 border-[.2px] text-black border-black rounded-lg' required/>
                 </div>
-                <button className='w-full p-3 focus:outline-none rounded-lg bg-color2 text-white ease-in duration-700 hover:bg-hovercolor text-center '>
+                <button type='submit' className='w-full p-3 rounded-lg bg-color2 text-white ease-in duration-700 hover:bg-hovercolor text-center'>
                   Send Message <i class="ri-send-plane-2-line"></i>
                 </button>
               </form>
@@ -42,5 +91,4 @@ const Conatact = () => {
     </div>
   )
 }
-
 export default Conatact
