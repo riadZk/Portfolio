b2d86d398cf203bb3c74c9479bde01bb17bd7456
Riad zakaria <124533233+riadZk@users.noreply.github.com>
2023-10-16 20:25:11 +0100

    Update Conatact.jsx

--- portfolio/src/components/main/Conatact.jsx
+++ portfolio/src/components/main/Conatact.jsx
@@ -43,26 +43,24 @@ const Conatact = () => {
     <div className={darkMode && 'dark'}>
       <section id='conatact' className='pb-16 dark:bg-Light bg-smallTextColor text-white dark:text-smallTextColor'>
         <div className='container'>
-          <h2 className='font-[400] text-center dark:text-smallTextColor text-white text-[2.5rem] mb-8'>Contact</h2>
+          <h2 className='font-[400] text-center dark:text-smallTextColor text-white text-[2.5rem] mb-8'>
+          Contact <span className='text-indigo-300'>Me</span></h2>
           <div className='md:flex justify-center '>
-            <div className='flex flex-col gap-5'>
-              <h2
-                data-aos="fade-right"
+            <div 
+              className='flex mb-5 flex-col gap-5'
+              data-aos="fade-right"
                 data-aos-delay="50"
-                data-aos-duration="1000"
+                data-aos-duration="2500" 
+              >
+              <h2
                 className='font-[400] text-center dark:text-smallTextColor text-white text-[1.5rem]'>Talk to me</h2>
               <div
-                data-aos="fade-right"
-                data-aos-duration="1500"
                 className='flex flex-col dark:bg-slate-100 bg-white text-smallTextColor py-3 px-10 rounded-lg place-items-center '>
                 <i class="ri-mail-send-fill"></i>
                 <span className='font-bold'>Email</span>
                 <p className='text-lg font-[500]'>riadzakaria48@gmail.com</p>
               </div>
               <div
-                data-aos="fade-right"
-                data-aos-delay="50"
-                data-aos-duration="1000"
                 className='flex flex-col dark:bg-slate-100 bg-white text-smallTextColor py-3 px-10 rounded-lg place-items-center '>
                 <MdOutlinePhoneIphone size={20} />
                 <span className='font-bold'>
@@ -72,10 +70,13 @@ const Conatact = () => {
               </div>
             </div>
             <div className='w-full rounded-lg  md:w-1/2 sm:h-[450px] lg:flex justify-center px-4 lg:px-8 '>
-              <form ref={form} onSubmit={sendEmail} className='w-full'
+              <form 
+                ref={form} onSubmit={sendEmail} 
+                className='w-full'
                 data-aos="fade-left"
                 data-aos-delay="50"
-                data-aos-duration="1000">
+                data-aos-duration="2500" 
+                >
                 <h2 className='font-[400] text-center dark:text-smallTextColor text-white text-[1.5rem] mb-5 '
                 >How can I help you ?</h2>
                 <div className='mb-5'>
